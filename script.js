077cbcd0d5ad331ddf3803fabc9fd81c6a8f7e5d
Caz <caroline.portilla@gmail.com>
2020-03-10 20:08:53 +0800

    buttons

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
+ 
 $(document).ready(function(){
 
-    var currentDay = $("#currentDay");
+  //Create variable for current hour
+ var currentHour = moment().format('h');
+ console.log(currentHour);
+ //Add text area to the timeblocks
+ var eventBlock =$(".col-md-10");
+ $(eventBlock).append("<textarea>");
+ var currentDay = $("#currentDay");
+
+ var hourBlock = $(".hour");
+ hourBlock = "";
+ 
     var update = function() {
     $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
     
-  }
+    }
     
     setInterval(update, 1000);
 
+
+    function setTimeblocks() {
+      
+      if(currentHour === hourBlock.value){
+        $(eventBlock).addClass("present");
+        
+    }
+  }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   });
+
+
+

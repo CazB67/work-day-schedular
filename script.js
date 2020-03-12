f92b2964b343274b191336b52cf5f50df59c91f5
Caz <caroline.portilla@gmail.com>
2020-03-12 20:24:24 +0800

    localstorage

--- script.js
+++ script.js
@@ -3,46 +3,43 @@ $(document).ready(function(){
   
   var rememberObj = [
     {
-      oclock: "9",
+      time: "9",
       reminder : " "
     },
     {
-      oclock: "10",
+      time: "10",
       reminder : " "
     },
     {
-      oclock: "11",
+      time: "11",
       reminder : " "
     },
     {
-      oclock: "12",
+      time: "12",
       reminder : " "
     },
     {
-      oclock: "13",
+      time: "13",
       reminder : " "
     },
     {
-      oclock: "14",
+      time: "14",
       reminder : " "
     },
     {
-      oclock: "15",
+      time: "15",
       reminder : " "
     },
     {
-      oclock: "16",
+      time: "16",
       reminder : " "
     },
     {
-      oclock: "17",
+      time: "17",
       reminder : " "
     }
   ];
-  
-  
-    //var reminderText = rememberObj[i].reminder;
-   // reminderText.text(textArea);
+
     renderDisplay();
  
   //Updates clock every second
@@ -62,8 +59,11 @@ function renderDisplay() {
   //Create variable for current hour
   var currentHour = moment().format('H');
   console.log("The current hour is " + currentHour);
+  rememberObj = JSON.parse(localStorage.getItem("reminders"));
 
   $(".container").empty();
+
+  //Loop to add all rows in array
   for (var i=0; i< rememberObj.length; i++) {
 
     //Row created and added to container
@@ -71,66 +71,51 @@ function renderDisplay() {
     rowX.addClass("row");
     $(".container").append(rowX);
 
-    //Column added to row
+    // 1st Column added to row
     var timeBlock = $("<div>");
     timeBlock.addClass("col-md-1 hour");
     $(rowX).append(timeBlock);
     
     var displayHour;
     //So the displayed hours are not in 24 hour time
-
-    if(rememberObj[i].oclock > 12) {
-      displayHour = (rememberObj[i].oclock - 12) + "pm";
-    }else if(parseInt(rememberObj[i].oclock) === 12){
-      displayHour = rememberObj[i].oclock + "pm"; 
+    if(rememberObj[i].time > 12) {
+      displayHour = (rememberObj[i].time - 12) + "pm";
+    }else if(parseInt(rememberObj[i].time) === 12){
+      displayHour = rememberObj[i].time + "pm"; 
     }else {
-      displayHour = rememberObj[i].oclock + "am";
+      displayHour = rememberObj[i].time + "am";
       }
       
       timeBlock.text(displayHour);
 
-    //column added to row
+    //2nd column added to row
     var eventDiv = $("<div>");
     eventDiv.addClass("col-md-10");
     $(rowX).append(eventDiv);
 
-    //text area added
+    //text area added to second column
     var textArea = $("<textarea>");
+    textArea.text(rememberObj[i].reminder);
     $(eventDiv).append(textArea);
-   
 
-    //Column for save button added
+    //Third Column for save button added
     var buttonDiv = $("<div>");
     buttonDiv.addClass("col-md-1 saveBtn");
     $(rowX).append(buttonDiv);
 
-    // Register the click -- for each button we have to have
-    // a way of identifying the what value we're looking at.
-    // As such we pass through an object, that contains what the 
-    // function RowClicked will use (in this case hours), and the value, in this case the rememberObj[i].oclock
+    //Creating event listener with some arguments
     $(buttonDiv).on("click", {
       inTimeBlock: displayHour,
       rowNumber: i,
       textAreaEl: textArea,
-      //eventToRemember : reminderText
     }, rowClicked );
 
-    //Row clicked has an 'event' argument passed into it.
-    // The event contains the object we created when we defined the .on click.
-    // In order to retrieve the info, javascript has an object heirarchy that uses event.data.
-  function rowClicked(event) {
-    console.log(event.data.rowNumber);
-    var reminderTextInputtedByMe = $(event.data.textAreaEl);
-    console.log(reminderTextInputtedByMe[0].value);
-
-    localStorage.setItem("reminders", JSON.stringify(reminderTextInputtedByMe[0].value));
-    
-  } //currentHour=13;
+  
     //Setting classes for past, present and future events
-    if(parseInt(currentHour) === parseInt(rememberObj[i].oclock)){
+    if(parseInt(currentHour) === parseInt(rememberObj[i].time)){
       eventDiv.addClass("present");
 
-    }else if(parseInt(currentHour) > parseInt(rememberObj[i].oclock)){
+    }else if(parseInt(currentHour) > parseInt(rememberObj[i].time)){
       eventDiv.addClass("past");
     }else{
       eventDiv.addClass("future");
@@ -138,6 +123,15 @@ function renderDisplay() {
     }
   }
 }
+
+function rowClicked(event) {
+  var reminderTextInput = $(event.data.textAreaEl);
+  console.log(reminderTextInput[0].value);
+  rememberObj[event.data.rowNumber].reminder = reminderTextInput[0].value;
+  localStorage.setItem("reminders", JSON.stringify(rememberObj));
+  
+  renderDisplay();
+} 
 
 });
 
